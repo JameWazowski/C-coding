ba7391104238c1a5675b0f63a98db4ed24cc5ccc
JameWazowski <jame.wazowski@gmail.com>
2024-08-31 10:42:56 +0300

    Arguments in function

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-void birthday() {
-    printf("\nHappy birtday to you!");
-    printf("\nHappy birtday to you!");
-    printf("\nHappy birthday dear...You!");
-    printf("\nHappy birtday to you!\n");
+void birthday(char x[], int y) {
+    printf("\nHappy birthday %s!", x);
+    printf("\nYou are %d years old!", y);
 }
 
 int main() {
+    char name[12];
+    int age;
 
-    birthday();
-    birthday();
-    birthday();
+    printf("Enter your first name: ");
+    scanf_s("%s", &name);
+
+    printf("Enter you age: ");
+    scanf("%d", &age);
+
+    birthday(name, age);
 
     return 0;
 }
