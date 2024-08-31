5efe21da855e3382ac6227760b189a17c6273b44
JameWazowski <jame.wazowski@gmail.com>
2024-08-31 10:30:45 +0300

    Introduction to functions

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,36 +1,17 @@
 #include <stdio.h>
-#include <stdbool.h>
+
+void birthday() {
+    printf("\nHappy birtday to you!");
+    printf("\nHappy birtday to you!");
+    printf("\nHappy birthday dear...You!");
+    printf("\nHappy birtday to you!\n");
+}
 
 int main() {
 
-    float temp = 25;
-    bool sunny = false;
-
-    // Logical 0perator AND &&
-
-    printf("This is a result of logical operator AND");
-    if (temp >= 0 && temp <= 30 && sunny) {
-        printf("\nThe weather is good!\n\n");
-    } else {
-        printf("\nThe weather is bad!\n\n");
-    }
-
-    // Logical Operator OR ||
-    printf("This is a result of logical operator OR");
-    if (temp <= 0 || temp >= 30) {
-        printf("\nThe weather is bad!\n\n");
-    } else {
-        printf("\nThe weather is good!\n\n");
-    }
-
-    // Logical Operator NOT !
-    bool rainy = true;
-    printf("This is a result of logical operator NOT");
-    if (!rainy) {
-        printf("\nIt\'s rainy outside!\n\n");
-    } else {
-        printf("\nIt\'s sunny outside!\n\n");
-    }
+    birthday();
+    birthday();
+    birthday();
 
     return 0;
 }
