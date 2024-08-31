16a0018c13dfeaefc56623df1e6408d43718d9e0
JameWazowski <jame.wazowski@gmail.com>
2024-08-31 13:20:17 +0300

    Ternary Operator

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-double square(double x) {
-    return x * x;
+int findMax(int x, int y) {
+    // if (x > y) {
+    //     return x;
+    // } else {
+    //     return y;
+    // }
+    return (x > y) ? x : y;
 }
 
 int main() {
 
-    double x = square(3.14);
-
-    printf("%lf", x);
+    int x = findMax(12, 65);
+    printf("%d", x);
 
     return 0;
 }
