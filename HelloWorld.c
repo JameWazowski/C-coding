c65d11271ceceb04b8d6258f79fa208345b15c30
JameWazowski <jame.wazowski@gmail.com>
2024-09-01 12:07:11 +0300

    Do-While loop

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char name[25];
+
+    int number = 0;
+    int sum = 0;
 
     do {
-        printf("\nWhat\'s your name? ");
-        fgets(name, 25, stdin);
-        name[strlen(name) - 1] = '\0';
-    } while (strlen(name) == 0);
+        printf("Enter a # above 0: ");
+        scanf("%d", &number);
+        if (number > 0) {
+            sum += number;
+        }
+    } while (number > 0);
 
-    printf("Hello %s", name);
+    printf("%d", sum);
 
     return 0;
 }
