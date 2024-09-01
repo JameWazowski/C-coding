01ab499f6247288fa9ac279689a7a1ed61643689
JameWazowski <jame.wazowski@gmail.com>
2024-09-01 11:55:55 +0300

    While loop

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    char name[25];
 
-    for (int i = 1; i <= 10; i++) {
-        printf("%d\n", i*i);
+    printf("\nWhat\'s your name? ");
+    fgets(name, 25, stdin);
+    name[strlen(name) - 1] = '\0';
+
+    while (strlen(name) == 0) {
+        printf("\nYou did not enter your name. Please try again.");
+        printf("\nWhat\'s your name? ");
+        fgets(name, 25, stdin);
+        name[strlen(name) - 1] = '\0';
     }
+
+    printf("Hello %s", name);
 
     return 0;
 }
