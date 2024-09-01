3eec0957ab9a9dd46f1e86855f27d2ec5bc1ba93
JameWazowski <jame.wazowski@gmail.com>
2024-09-01 12:01:07 +0300

    Using do-while instead of while loop

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,16 +4,11 @@
 int main() {
     char name[25];
 
-    printf("\nWhat\'s your name? ");
-    fgets(name, 25, stdin);
-    name[strlen(name) - 1] = '\0';
-
-    while (strlen(name) == 0) {
-        printf("\nYou did not enter your name. Please try again.");
+    do {
         printf("\nWhat\'s your name? ");
         fgets(name, 25, stdin);
         name[strlen(name) - 1] = '\0';
-    }
+    } while (strlen(name) == 0);
 
     printf("Hello %s", name);
 
