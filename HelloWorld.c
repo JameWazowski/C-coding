35336c87c306e478f1c20883770bc76d7cefc07c
JameWazowski <jame.wazowski@gmail.com>
2024-09-02 15:44:49 +0300

    Print an array with loop

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,36 +1,15 @@
 #include <stdio.h>
 
-// arrays
-
 int main() {
 
-    double prices1[] = {5.0, 10.0, 15.0, 25.0, 20.0};
+    double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0, 30.0};
 
-    printf("First array\n");
-    printf("-----------\n");
+    printf("\nSize of array - %d bytes.\n", sizeof(prices));
 
-    printf("First price  - $%.2lf\n", prices1[0]);
-    printf("Second price - $%.2lf\n", prices1[1]);
-    printf("Third price  - $%.2lf\n", prices1[2]);
-    printf("Fourth price - $%.2lf\n", prices1[3]);
-    printf("Fifth price  - $%.2lf\n\n", prices1[4]);
-
-    double prices2[5];
-
-    prices2[0] = 10.0;
-    prices2[1] = 20.0;
-    prices2[2] = 30.0;
-    prices2[3] = 50.0;
-    prices2[4] = 40.0;
-
-    printf("Second array\n");
-    printf("------------\n");
-
-    printf("First price  - $%.2lf\n", prices2[0]);
-    printf("Second price - $%.2lf\n", prices2[1]);
-    printf("Third price  - $%.2lf\n", prices2[2]);
-    printf("Fourth price - $%.2lf\n", prices2[3]);
-    printf("Fifth price  - $%.2lf\n\n", prices2[4]);
+    printf("Price array - ");
+    for (int i = 0; i < sizeof(prices)/sizeof(double); i++) {
+        printf("$%.2lf ", prices[i]);
+    }
 
     return 0;
 }
