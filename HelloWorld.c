0d413249a71cadd9f575e38e852e937634c7ff5e
JameWazowski <jame.wazowski@gmail.com>
2024-09-03 23:18:13 +0300

    typedef in C

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Player {
-    char name[12];
-    int score;
-};
+typedef struct {
+    char name[25];
+    char password[16];
+    int id;
+} User;
 
 int main() {
-    // struct
 
-    struct Player player1;
-    struct Player player2;
+    User user1 = {"Jame", "123456789", 12345};
+    User user2 = {"Wazowski", "987654321", 93423};
 
-    strcpy(player1.name, "Jame");
-    player1.score = 8;
-
-    strcpy(player2.name, "Wazowski");
-    player2.score = 10;
-
-    printf("Player 1\t|\t Score\n");
-    printf("%s\t\t|\t    %d\n", player1.name, player1.score);
-    printf("%s\t|\t   %d\n", player2.name, player2.score);
+    printf("User 1 - %s, %s, %d\n", user1.name, user1.password, user1.id);
+    printf("User 2 - %s, %s, %d\n", user2.name, user2.password, user2.id);
 
     return 0;
 }
