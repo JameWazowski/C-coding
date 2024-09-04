c0058b3472d5635327f6ced41101a6382eeef2a8
JameWazowski <jame.wazowski@gmail.com>
2024-09-04 10:54:07 +0300

    Enum in C

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
-struct Student {
-    char name[12];
-    float gpa;
-};
+enum Day{Mon = 1, Tue = 2, Wed = 3, Thu = 4, Fri = 5, Sat = 6, Sun = 7};
 
 int main() {
 
-    struct Student student1 = {"Alice", 3.0};
-    struct Student student2 = {"Bob", 2.5};
-    struct Student student3 = {"Eva", 3.5};
-    struct Student student4 = {"Jame", 4.0};
+    enum Day today = Wed;
 
-    struct Student students[] = {student1, student2, student3, student4};
-
-    printf("\nsizeof(students) = %d\n", sizeof(students));
-    printf("sizeof(students[0]) = %d\n", sizeof(students[0]));
-    printf("So, quantity of array elements = %d\n", sizeof(students)/sizeof(students[0]));
-    printf("==================================\n");
-
-    for (int i = 0; i < sizeof(students)/sizeof(students[0]); i++) {
-        printf("Student %d\t-\t%s\t-\t%.2f\n", i+1, students[i].name, students[i].gpa);
+    if (today == Sat || today == Sun) {
+        printf("Today is Weekend!");
+    } else {
+        printf("I have to work today");
     }
-
-    printf("\n");
 
     return 0;
 }
