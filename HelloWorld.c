47d4297500a296cea99caef0998d9741971462df
JameWazowski <jame.wazowski@gmail.com>
2024-09-05 18:50:16 +0300

    Pointers

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 
+void printAge(int age) {
+    printf("You are %d years old\n", age);
+}
+
+void printWithPointerAge(int *pAge) {
+    printf("You are %d years old\n", *pAge);
+}
+
 int main() {
+    // Advantages of using pointers:
+    // 1. Less time in program execution
+    // 2. Working on the original variable
+    // 3. With the help of pointers, we can create data structures (linked-list, stack, queue)
+    // 4. Returning more than one values from functions
+    // 5. Searching and sorting large data very easily
+    // 6. Dynamically memory allocation
 
-    double a = 'X';
-    double b = 'Y';
-    double c = 'Z';
+    int age = 21;
+    int *pAge = NULL; // good practice to assign NULL if declaring a pointer
+    pAge = &age;
 
-    printf("SIZES\n");
-    printf("a = %d bytes\n", sizeof(a));
-    printf("b = %d bytes\n", sizeof(b));
-    printf("c = %d bytes\n\n", sizeof(c));
+    printf("\naddress of age:  %p\n", &age);
+    printf("value of pAge:   %p\n\n", pAge);
 
-    printf("ADDRESSES\n");
-    printf("a = %p\n", &a);
-    printf("b = %p\n", &b);
-    printf("c = %p\n", &c);
+    printf("value of age:    %d\n", age);
+    printf("value at stored address (from pAge): %d\n\n", *pAge);
 
+    printf("Size of variable age:  %d bytes\n", sizeof(age));
+    printf("Size of variable pAge: %d bytes\n", sizeof(pAge));
 
-    printf("ARRAY OF DOUBLES\n");
-    double d;
-    double e[10];
-    printf("size of type double = %d;\naddress of double variable = %p\n", sizeof(double), &d);
-    printf("size of array of doubles = %d;\naddress of array of doubles = %p", sizeof(e), &e);
+    printf("\nWORKING WITH FUNCTIONS\n");
+    printAge(age);
+    printWithPointerAge(pAge);
 
+    printf("\n");
+    
     return 0;
 }
