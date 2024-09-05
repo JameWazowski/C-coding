a482eb724f771970d147354e0f5fe3d2d9b5c7c7
JameWazowski <jame.wazowski@gmail.com>
2024-09-05 17:34:08 +0300

    Memory addresses

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,54 +2,26 @@
 
 int main() {
 
-    // & = AND
-    // | = OR
-    // ^ = XOR
-    // <<  left shift
-    // >> right shift
+    double a = 'X';
+    double b = 'Y';
+    double c = 'Z';
 
-    int x = 6;  //  6 = 00000110
-    int y = 12; // 12 = 00001100
-    int z = 0;  //  0 = 00000000
+    printf("SIZES\n");
+    printf("a = %d bytes\n", sizeof(a));
+    printf("b = %d bytes\n", sizeof(b));
+    printf("c = %d bytes\n\n", sizeof(c));
 
-    printf("SIMPLE EXAMPLES:\n");
-    z = x & y;
-    printf("x & y = %d\n", z);
+    printf("ADDRESSES\n");
+    printf("a = %p\n", &a);
+    printf("b = %p\n", &b);
+    printf("c = %p\n", &c);
 
-    z = x | y;
-    printf("x | y = %d\n", z);
 
-    z = x ^ y;
-    printf("x ^ y = %d\n", z);
+    printf("ARRAY OF DOUBLES\n");
+    double d;
+    double e[10];
+    printf("size of type double = %d;\naddress of double variable = %p\n", sizeof(double), &d);
+    printf("size of array of doubles = %d;\naddress of array of doubles = %p", sizeof(e), &e);
 
-    z = x << 1;
-    printf("%d = x << 1\n", z);
-
-    z = x >> 1;
-    printf("%d = x >> 1\n", z);
-
-    z = y << 1;
-    printf("%d = y << 1\n", z);
-
-    z = y >> 1;
-    printf("%d = y >> 1\n", z);
-
-    printf("\nLEFT SHIFT (MULTIPLICATION):\n");
-    int a = 5;
-    int b = 0;
-    printf("Our a = %d, b = %d\n", a, b);
-    for (int i = 0; i < 10; i++) {
-        b = a << i;
-        printf("%d = a << %d\t\n", b, i);
-    }
-
-    printf("\nRIGHT SHIFT (DIVISION):\n");
-    int c = 10000;
-    int d = 0;
-    printf("Our c = %d, d = %d\n", c, d);
-    for (int i = 0; i < 10; i++) {
-        d = c >> i;
-        printf("%d = c >> %d\n", d, i);
-    }
     return 0;
 }
