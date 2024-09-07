56d6b90664c115aa7b2fdabfdd8d242648670e09
JameWazowski <jame.wazowski@gmail.com>
2024-09-07 13:26:55 +0300

    Delete the if-statement block and modify the switch

--- Head-First-C/Chapter 1/EvaluateFaceValues.c
+++ Head-First-C/Chapter 1/EvaluateFaceValues.c
@@ -10,25 +10,10 @@ int main() {
     scanf("%2s", card_name);
 
     int val = 0;
-    // if (card_name[0] == 'K') {
-    //     val = 10;
-    // } else if (card_name[0] == 'Q') {
-    //     val = 10;
-    // } else if (card_name[0] == 'J') {
-    //     val = 10;
-    // } else if (card_name[0] == 'A') {
-    //     val = 11;
-    // } else {
-    //     val = atoi(card_name); // atoi() - converts the text into a number
-    // }
 
     switch (card_name[0]) {
         case 'J':
-            val = 10;
-            break;
         case 'Q':
-            val = 10;
-            break;
         case 'K':
             val = 10;
             break;
