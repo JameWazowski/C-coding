e8a613d6f2ba328c130f850cc963f138a8fa8345
JameWazowski <jame.wazowski@gmail.com>
2024-09-07 12:31:49 +0300

    Modifying for card counting

--- Head-First-C/Chapter 1/EvaluateFaceValues.c
+++ Head-First-C/Chapter 1/EvaluateFaceValues.c
@@ -22,7 +22,12 @@ int main() {
         val = atoi(card_name); // atoi() - converts the text into a number
     }
 
-    printf("The card value is: %i\n", val);
+    // Modifying for card counting
+    if (val >= 3 && val <= 6) {
+        puts("Count has gone up");
+    } else if (val == 10) {
+        puts("Count has gone down");
+    }
 
     return 0;
 }
