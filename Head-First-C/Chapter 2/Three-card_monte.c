eeca0293d5e1f16e8f52fb765c19880581effe76
JameWazowski <jame.wazowski@gmail.com>
2024-09-11 12:20:46 +0300

    Three-card monte

---
+++ Head-First-C/Chapter 2/Three-card_monte.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    char *cards = "JQK";
+    char a_card = cards[2];
+    cards[2] = cards[1];
+    cards[1] = cards[0];
+    cards[0] = cards[2];
+    cards[2] = cards[1];
+    cards[1] = a_card;
+    
+    for (int i = 0; i < 3; i++)
+        printf("%s\n", cards[i]);
+
+    return 0;
+}
+
+// This code does not have any output because the string can't be updated
