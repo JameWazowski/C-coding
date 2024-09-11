ed87334c39b3e32ff8e8491dfb7b1fad0f87f6e7
JameWazowski <jame.wazowski@gmail.com>
2024-09-11 13:05:17 +0300

    Using a copy of string instead of a string

--- Head-First-C/Chapter 2/Three-card_monte.c
+++ Head-First-C/Chapter 2/Three-card_monte.c
@@ -2,7 +2,9 @@
 
 int main() {
 
-    char *cards = "JQK";
+    // char *cards = "JQK";
+
+    char cards[] = "JQK";
     char a_card = cards[2];
     cards[2] = cards[1];
     cards[1] = cards[0];
@@ -11,7 +13,7 @@ int main() {
     cards[1] = a_card;
     
     for (int i = 0; i < 3; i++)
-        printf("%s\n", cards[i]);
+        printf("%c\n", cards[i]);
 
     return 0;
 }
