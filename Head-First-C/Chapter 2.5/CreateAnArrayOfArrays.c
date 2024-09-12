e4a7850a7b0d5c586e39bb69ac18ddd48b521d85
JameWazowski <jame.wazowski@gmail.com>
2024-09-12 19:19:47 +0300

    Searching song by word

--- Head-First-C/Chapter 2.5/CreateAnArrayOfArrays.c
+++ Head-First-C/Chapter 2.5/CreateAnArrayOfArrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char tracks[][80] = {
@@ -9,7 +10,6 @@ int main() {
         "The girl from Iwo Jima"
     };
 
-
     printf("List of songs:\n");
     for (int i = 0; i < 5; i++) {
         printf("%i. %s\n", i+1, tracks[i]);
@@ -18,6 +18,17 @@ int main() {
     printf("\nAddresses of songs:\n");
     for (int i = 0; i < 5; i++) {
         printf("Song number %i is allocated at %p\n", i+1, &tracks[i]);
+    }
+
+    char suggestion[20];
+    printf("Please enter the word from song name: ");
+    // fgets(suggestion, sizeof(suggestion), stdin);
+    scanf("%19s", suggestion);
+    
+    for (int i = 0; i < 5; i++) {
+        if (strstr(tracks[i], suggestion)) {
+            printf("Song number %i have word \"%s\" - %s\n", i+1, suggestion, tracks[i]);
+        }
     }
 
     return 0;
