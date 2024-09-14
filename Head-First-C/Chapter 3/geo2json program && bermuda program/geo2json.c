5fba06fbc2a080169df4d7f949fa15bf8c3d81c4
JameWazowski <jame.wazowski@gmail.com>
2024-09-14 12:59:03 +0300

    Using two small tools together with pipe

---
+++ Head-First-C/Chapter 3/geo2json program && bermuda program/geo2json.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+
+    float latitude;
+    float longitude;
+    char info[80];
+    int started = 0;
+
+    puts("{");
+    puts("\t\"data\": [");  
+    while (scanf("%f,%f,%79s\n", &latitude, &longitude, info) == 3) {
+        
+        if (latitude < -90.0 || latitude > 90.0) {
+            fprintf(stderr, "Error message: latitude is %f\n", latitude);
+            puts("\n\t]");
+            puts("}");
+            return 2;
+        }
+
+        if (longitude < -180.0 || longitude > 180.0) {
+            fprintf(stderr, "Error message: longitude is %f\n", longitude);
+            puts("\n\t]");
+            puts("}");
+            return 2;
+        }
+        
+        if (started)
+            printf(",\n");
+        else 
+            started = 1;
+
+        printf("\t\t{\"latitude\": %f, \"longitude\": %f, \"info\": \"%s\"}", latitude, longitude, info);
+    }
+
+    puts("\n\t]");
+    puts("}");
+
+    return 0;
+}
