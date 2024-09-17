ba1216a9c097033d8104d88c9f2c94b8356bbee9
JameWazowski <jame.wazowski@gmail.com>
2024-09-17 11:50:39 +0300

    Be The Compiler (case 4)

--- Head-First-C/Chapter 4/BeTheCompiler_exercise.c
+++ Head-First-C/Chapter 4/BeTheCompiler_exercise.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+float mercury_day_in_earth_days();
+int hours_in_an_earth_day();
+
 int main() {
-    float length_of_day = mercury_day_in_earth_days();
+    int length_of_day = mercury_day_in_earth_days();
     int hours = hours_in_an_earth_day();
     float day = length_of_day * hours;
     printf("A day on Mercury is %f hours\n", day);
@@ -16,6 +19,6 @@ int hours_in_an_earth_day() {
     return 24;
 }
 
-// You can compile this code -
-// You should display a warning +
+// You can compile this code +
+// You should display a warning -
 // The program will work -
