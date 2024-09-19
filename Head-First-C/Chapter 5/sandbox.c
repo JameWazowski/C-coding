e1e69f495337a01237dfb21ce5c5bf7d87b75a56
JameWazowski <jame.wazowski@gmail.com>
2024-09-19 16:35:19 +0300

    Try some things with structs

--- Head-First-C/Chapter 5/sandbox.c
+++ Head-First-C/Chapter 5/sandbox.c
@@ -15,19 +15,29 @@ void label(struct fish f) {
     printf("Name: %s\nSpecies: %s\nTeeth: %i\nAge: %i\n", f.name, f.species, f.teeth, f.age);
 }
 
-// void catalog(const char *name, const char *species, int teeth, int age) {
-//     printf("%s is a %s with %i teeth. He is %i\n", name, species, teeth, age);
-// }
-
-// void label(const char *name, const char *species, int teeth, int age) {
-//     printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n", name, species, age, teeth);
-// }
-
 int main() {
 
     struct fish snappy = {"Snappy", "Piranha", 69, 4}; 
     catalog(snappy);
     label(snappy);
+
+    puts("");
+    struct fish gnasher = snappy;
+    catalog(gnasher);
+    label(gnasher);
+
+    printf("Address of Snappy name - \t\t\t%p\nAddress of Gnasher name - \t\t\t%p\n\n", snappy.name, gnasher.name);
+
+    gnasher.name = "Gnasher";
+
+    printf("The address of snappy.name (%s) - \t\t%p\n", snappy.name, snappy.name);
+    printf("The address of gnasher.name (%s) - \t%p\n\n", gnasher.name, gnasher.name);
+
+    printf("Age of %s is %i at address %p\n", snappy.name, snappy.age, &snappy.age);
+    printf("Age of %s is %i at address %p\n\n", gnasher.name, gnasher.age, &gnasher.age);
+
+    printf("Size of Snappy struct is  %i\n", sizeof(snappy));
+    printf("Size of Gnasher struct is %i\n", sizeof(gnasher));
 
     return 0;
 }
