ee214a748de2324a299d8b0b41de453c2c404b19
JameWazowski <jame.wazowski@gmail.com>
2024-09-19 15:32:59 +0300

    Handle data using structs

--- Head-First-C/Chapter 5/sandbox.c
+++ Head-First-C/Chapter 5/sandbox.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 
-void catalog(const char *name, const char *species, int teeth, int age) {
-    printf("%sw is a %s with %i teeth. He is %i\n", name, species, teeth, age);
+struct fish {
+    const char *name;
+    const char *species;
+    int teeth;
+    int age;
+};
+
+void catalog(struct fish f) {
+    printf("%s is a %s with %i. He is %i\n", f.name, f.species, f.teeth, f.age);
 }
 
-void label(const char *name, const char *species, int teeth, int age) {
-    printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n", name, species, age, teeth);
+void label(struct fish f) {
+    printf("Name: %s\nSpecies: %s\nTeeth: %i\nAge: %i\n", f.name, f.species, f.teeth, f.age);
 }
+
+// void catalog(const char *name, const char *species, int teeth, int age) {
+//     printf("%s is a %s with %i teeth. He is %i\n", name, species, teeth, age);
+// }
+
+// void label(const char *name, const char *species, int teeth, int age) {
+//     printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n", name, species, age, teeth);
+// }
 
 int main() {
 
-    catalog("Snappy", "Piranha", 69, 4);
-    label("Snappy", "Piranha", 69, 4);
+    struct fish snappy = {"Snappy", "Piranha", 69, 4}; 
+    catalog(snappy);
+    label(snappy);
 
     return 0;
 }
