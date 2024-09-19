e854fc6cb77584f7a49f19aa51813c972f7b8db2
JameWazowski <jame.wazowski@gmail.com>
2024-09-19 17:48:41 +0300

    Long Exercise (part 2)

--- Head-First-C/Chapter 5/long_exercise.c
+++ Head-First-C/Chapter 5/long_exercise.c
@@ -23,21 +23,29 @@ struct fish {
     struct preferences care;
 };
 
-void output_about_fish(struct fish f) {
+void output_about_fish_first_task(struct fish f) {
     // printf("Name: %s\nSpecies: %s\nFood ingredients: %s\nFood weight: %.1f lbs\nExcercise description: %s\nExersice duration %.1f hours\n", f.name, f.species, f.care.food.ingredients, f.care.food.weight, f.care.exercise.decription, f.care.exercise.duration);
 
+    printf("First exercise:\n");
     printf("Name: %s\n", f.name);
     printf("Species: %s\n", f.species);
     printf("Food ingredients: %s\n", f.care.food.ingredients);
     printf("Food weight: %.1f lbs\n", f.care.food.weight);
     printf("Exercise decription: %s\n", f.care.exercise.decription);
-    printf("Exercise duration %.1f hours\n", f.care.exercise.duration);
+    printf("Exercise duration %.1f hours\n\n", f.care.exercise.duration);
+}
+
+void label(struct fish a) { // Second task
+    printf("Second exercise:\n");
+    printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n", a.name, a.species, a.age, a.teeth);
+    printf("Feed with %.2f lbs of %s and allow to %s for %.2f hours\n\n", a.care.food.weight, a.care.food.ingredients, a.care.exercise.decription, a.care.exercise.duration);
 }
 
 int main() {
     
-    struct fish snappy = {"Snappy", "Piranha", 0, 0, {{"meat", 0.2}, {"swim in the jacuzzi", 7.5}}};
-    output_about_fish(snappy);
+    struct fish snappy = {"Snappy", "Piranha", 69, 4, {{"meat", 0.2}, {"swim in the jacuzzi", 7.5}}};
+    output_about_fish_first_task(snappy);
+    label(snappy);
 
     return 0;
 }
