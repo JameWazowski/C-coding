0539e262288186b7d10a79a2c5846ec5caa71f6d
JameWazowski <jame.wazowski@gmail.com>
2024-09-20 20:31:45 +0300

    Storing struct elements in memory

--- Head-First-C/Chapter 5/exercise.c
+++ Head-First-C/Chapter 5/exercise.c
@@ -19,5 +19,24 @@ int main() {
     diver randy = {"Randy", {5.5, 3500, "Neoprene"}};
     badge(randy);
 
+    printf("\nAddresses of struct elements: \n");
+    printf("The address of %s is \t%p\n", randy.name, randy.name);
+    printf("The address of %.1f is \t\t%p\n", randy.kit.tank_capacity, &randy.kit.tank_capacity);
+    printf("The address of %i is \t\t%p\n", randy.kit.tank_psi, &randy.kit.tank_psi);
+    printf("The address of %s is \t%p\n", randy.kit.suit_material, randy.kit.suit_material);
+    // Conclusion: elements in the structure are stored relative to their data types
+
+    const char *pName = randy.name;
+    float *pTankCapacity = &randy.kit.tank_capacity;
+    int *pTankPsi = &randy.kit.tank_psi;
+    const char *pSuitMaterial = randy.kit.suit_material;
+
+    printf("\nAddresses of pointers:\n");
+    printf("The address of pointer %s in \t%p\n", randy.name, &pName);
+    printf("The address of pointer %.1f in \t\t%p\n", randy.kit.tank_capacity, &pTankCapacity);
+    printf("The address of pointer %i in \t\t%p\n", randy.kit.tank_psi, &pTankPsi);
+    printf("The address of pointer %s in \t%p\n", randy.kit.suit_material, &pSuitMaterial);
+    // Conclusion: pointers are stored in memory sequentially
+
     return 0;
 }
