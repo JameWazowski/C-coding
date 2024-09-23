637467e7730962f575c06aff2bee1686620534a9
JameWazowski <jame.wazowski@gmail.com>
2024-09-23 23:03:34 +0300

    Insert new struct into linked list

--- Head-First-C/Chapter 6/islands_linked_list.c
+++ Head-First-C/Chapter 6/islands_linked_list.c
@@ -20,9 +20,23 @@ int main() {
     isla_nublar.next = &shutter;
 
     printf("Linked List\n");
-    printf("%s island is allocated at \t\t%p\n", amity.name, &amity);
-    printf("%s island is allocated at \t\t%p\n", craggy.name, &craggy);
-    printf("%s island is allocated at \t%p\n", isla_nublar.name, &isla_nublar);
+    printf("%s island is allocated at \t\t%p and points to %p\n", amity.name, &amity, amity.next);
+    printf("%s island is allocated at \t\t%p and points to %p\n", craggy.name, &craggy, craggy.next);
+    printf("%s island is allocated at \t%p and points to %p\n", isla_nublar.name, &isla_nublar, isla_nublar.next);
+    printf("%s island is allocated at \t\t%p\n", shutter.name, &shutter);
+
+    // Create new island 
+    island skull = {"Skull", "09:00", "17:00", NULL};
+
+    // Insert new island between Isla Nublar and Shutter
+    isla_nublar.next = &skull;
+    skull.next = &shutter;
+
+    printf("\nLinked List (after insert)\n");
+    printf("%s island is allocated at \t\t%p and points to %p\n", amity.name, &amity, amity.next);
+    printf("%s island is allocated at \t\t%p and points to %p\n", craggy.name, &craggy, craggy.next);
+    printf("%s island is allocated at \t%p and points to %p\n", isla_nublar.name, &isla_nublar, isla_nublar.next);
+    printf("%s island is allocated at \t\t%p and points to %p\n", skull.name, &skull, skull.next);
     printf("%s island is allocated at \t\t%p\n", shutter.name, &shutter);
 
     return 0;
