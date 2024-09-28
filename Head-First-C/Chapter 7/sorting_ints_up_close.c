b78fedd8acaf160011cd8e4032969d8d010a298b
JameWazowski <jame.wazowski@gmail.com>
2024-09-28 10:57:57 +0300

    Using qsort() function with comparator functions

--- Head-First-C/Chapter 7/sorting_ints_up_close.c
+++ Head-First-C/Chapter 7/sorting_ints_up_close.c
@@ -16,6 +16,24 @@ int compare_names_desc(const void*, const void*);
 
 int main() {
 
+    int scores[] = {543,323,32,554,11,3,112};
+    int i;
+    qsort(scores, 7, sizeof(int), compare_scores);
+    puts("\nThese are the scores in order:");
+    for (i = 0; i < 7; i++)
+        printf("Score = %i\n", scores[i]);
+
+    qsort(scores, 7, sizeof(int), compare_scores_desc);
+    puts("\nThese are the scores in reversed order");
+    for (i = 0; i < 7; i++)
+        printf("Score = %i\n", scores[i]);
+
+    char *names[] = {"Karen", "Mark", "Brett", "Molly"};
+    qsort(names, 4, sizeof(char*), compare_names);
+    puts("These are the names in order:");
+    for (i = 0; i < 4; i++)
+        printf("%s\n", names[i]);
+    
     return 0;
 }
 
