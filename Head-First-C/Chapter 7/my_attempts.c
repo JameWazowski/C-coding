3bdb77cd084d7a0ce9d1c343153db4acddf2e767
JameWazowski <jame.wazowski@gmail.com>
2024-09-28 11:58:06 +0300

    Making a animals comparator by my own

--- Head-First-C/Chapter 7/my_attempts.c
+++ Head-First-C/Chapter 7/my_attempts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int compare_nums(const void*, const void *);
+int compare_animals(const void* first_animal, const void* second_animal);
 
 int main() {
 
@@ -15,6 +17,17 @@ int main() {
         printf("%i\n", num_sequence[i]);
     }
 
+    puts("--------------------------------------------------");
+
+    char *animals[] = {"Lion", "Zebra", "Elephant", "Shark", "Ant", "Bee"};
+    size_t q = 6;
+
+    qsort(animals, q, sizeof(char*), compare_animals);
+
+    puts("Make an animal order:");
+    for (int i = 0; i < q; i++)
+        printf("%s\n", animals[i]);
+
     return 0;
 }
 
@@ -22,4 +35,10 @@ int compare_nums(const void* a_num, const void* b_num) {
     int a = *(int*)a_num;
     int b = *(int*)b_num;
     return a - b;
+}
+
+int compare_animals(const void* first_animal, const void* second_animal) {
+    char** anim_one = (char**)first_animal;
+    char** anim_two = (char**)second_animal;
+    return strcmp(*anim_one, *anim_two);
 }
---
+++ Head-First-C/Chapter 7/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+
+    puts("Make an animal order:");
