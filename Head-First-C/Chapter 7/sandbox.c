9da455e6de9b2ce64beb9f44cd9ddd30c183e652
JameWazowski <jame.wazowski@gmail.com>
2024-09-29 23:18:59 +0300

    Add a new function and add it to the array of function pointers

--- Head-First-C/Chapter 7/sandbox.c
+++ Head-First-C/Chapter 7/sandbox.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-enum response_type {DUMP, SECOND_CHANCE, MARRIAGE};
+enum response_type {DUMP, SECOND_CHANCE, MARRIAGE, DIVORCE};
 typedef struct {
     char *name;
     enum response_type type;
@@ -9,17 +9,25 @@ typedef struct {
 void dump(response);
 void second_chance(response);
 void marriage(response);
+void divorce(response);
 
-void (*replies[])(response) = {dump, second_chance, marriage};
+void (*replies[])(response) = {dump, second_chance, marriage, divorce};
 
 int main() {
     
     response r[] = {
         {"Mike", DUMP}, {"Luis", SECOND_CHANCE}, 
-        {"Matt", SECOND_CHANCE}, {"William", MARRIAGE}
+        {"Matt", SECOND_CHANCE}, {"William", MARRIAGE},
+        {"Kenny", DIVORCE}, {"Bob", DIVORCE}
     };
+    size_t r_length = sizeof(r)/sizeof(r[0]);
 
-    for (int i = 0; i < 4; i++)
+    puts("Sizes:");
+    printf("sizeof(r) = %i\n", sizeof(r));
+    printf("sizeof(r[0]) = %i\n", sizeof(r[0]));
+    printf("size_t r_length = %i\n", r_length);
+
+    for (int i = 0; i < r_length; i++)
         (replies[r[i].type])(r[i]);
 
     return 0;
@@ -28,17 +36,23 @@ int main() {
 void dump(response r) {
     printf("Dear %s,\n", r.name);
     puts("Unfortunately your last date contacted us to");
-    puts("say that they will not be seeing you again");
+    puts("say that they will not be seeing you again\n");
 }
 
 void second_chance(response r) {
     printf("Dear %s,\n", r.name);
     puts("Good news: your last date has asked us to");
-    puts("arrange another meeting. Please call ASAP.");
+    puts("arrange another meeting. Please call ASAP.\n");
 }
 
 void marriage(response r) {
     printf("Dear %s,\n", r.name);
     puts("Congratulations! Your last date has contacted");
-    puts("us with a proposal of marriage.");
+    puts("us with a proposal of marriage.\n");
+}
+
+void divorce(response r) {
+    printf("Dear %s,\n", r.name);
+    puts("Your wife wanna go away from you");
+    puts("Bye!\n");
 }
